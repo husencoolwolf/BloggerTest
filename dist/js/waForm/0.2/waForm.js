0f01ad4cc6acb245643d479ecf2f9c59cd51102b
husencoolwolf <52161767+husencoolwolf@users.noreply.github.com>
2020-10-28 16:28:05 +0700

    Add files via upload

---
+++ dist/js/waForm/0.2/waForm.js
@@ -0,0 +1,6 @@
+function waForm(){
+    var nama = document.getElementById('wa-nama');
+    var kota = document.getElementById('wa-kota');
+    window.location.replace("https://api.whatsapp.com/send?phone=6285695611430&text=Halo%2C%20Nama%20saya%20"+nama+"%2C%20Saya%20dari%20"+kota+".%0AIngin%20bertanya%20tentang%20produk%20Andalani%20%3F!");
+
+}
